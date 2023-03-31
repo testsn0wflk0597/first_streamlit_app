e20e499b5f41eda76cb60ac53243c2602ba3cb5e
testsn0wflk0597 <129216371+testsn0wflk0597@users.noreply.github.com>
2023-03-31 16:19:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from urllib.error import URLError
 
 streamlit.title('Testing with Python')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["GARDEN_VEGGIES"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["garden_veggies"])
 my_cur = my_cnx.cursor()
 query_result = my_cur.execute("SELECT * FROM GARDEN_PLANTS.VEGGIES.VEGETABLE_DETAILS")
 streamlit.dataframe(query_result)
